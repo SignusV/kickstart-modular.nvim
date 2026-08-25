78e8ef7216033cbf406d415e271e166420f01dfc
Khai Sian, Victor Lim <victor.ks.lim@seagate.com>
2026-08-25 14:13:00 +0800

    New Theme

---
+++ lua/custom/plugins/themeKanagawa.lua
@@ -0,0 +1,11 @@
+vim.pack.add({
+  {
+    src = 'https://github.com/rebelot/kanagawa.nvim',
+  },
+})
+
+require('kanagawa').setup({
+  theme = 'dragon',
+})
+
+vim.cmd.colorscheme('kanagawa-dragon')
